8a661617173898c363eabca38902205d4f71161c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-04 14:53:16 +0530

    initialize the on object with the method call of the method findViewById()

--- app/src/main/java/com/example/flashlight_app_java/MainActivity.java
+++ app/src/main/java/com/example/flashlight_app_java/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.flashlight_app_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.widget.Button;
@@ -14,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        on = findViewById(R.id.button);
     }
 }

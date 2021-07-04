1bcecd89962319b371ff608380bc22e53d36d2b2
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-04 14:53:48 +0530

    initialize the off object with the method call of the method findViewById()

--- app/src/main/java/com/example/flashlight_app_java/MainActivity.java
+++ app/src/main/java/com/example/flashlight_app_java/MainActivity.java
@@ -16,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         on = findViewById(R.id.button);
+        off = findViewById(R.id.button2);
     }
 }

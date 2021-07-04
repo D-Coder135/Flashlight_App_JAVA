d6a85e2f98c0e6ef020a055e6790e27b38269eef
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-04 14:56:43 +0530

    initialize the object cameraManager

--- app/src/main/java/com/example/flashlight_app_java/MainActivity.java
+++ app/src/main/java/com/example/flashlight_app_java/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.flashlight_app_java;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -11,11 +13,13 @@ public class MainActivity extends AppCompatActivity {
     Button on, off;
     CameraManager cameraManager;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         on = findViewById(R.id.button);
         off = findViewById(R.id.button2);
+        cameraManager = (CameraManager)getSystemService(CAMERA_SERVICE);
     }
 }

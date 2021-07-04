e0079340a5b9a967fd9a9e9c9651e6e52cd74916
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-04 14:47:42 +0530

    define the objects for the Button palette

--- app/src/main/java/com/example/flashlight_app_java/MainActivity.java
+++ app/src/main/java/com/example/flashlight_app_java/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.flashlight_app_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button on, off;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
